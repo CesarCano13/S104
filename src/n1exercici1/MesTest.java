19e963326fb1e7fbbed997849d00e3c34da2a969
CesarCano13 <cesarbroock13@gmail.com>
2022-07-12 21:09:37 +0200

    act

--- src/n1exercici1/MesTest.java
+++ src/n1exercici1/MesTest.java
@@ -2,26 +2,38 @@ package n1exercici1;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class MesTest {
 
-	
+	ArrayList<String> meses = new ArrayList<String>();
 
-	@Test
-	void testComprobarSize() {
-		int size = Mes.getMeses().size();
-		int esperado = 12;
-		assertEquals(size, esperado);
+	//Para inicializar la clase del Array y guardarlo en una variable
+	@BeforeEach
+	void inicializar() {
+		meses = Mes.crearArray();
 	}
-	
+
+	// Comprobamos que el tamaño es 12
 	@Test
-	void testComprobarAgosto() {
-		if (Mes.getMeses().get(7).equalsIgnoreCase("Agosto") ) {
-			System.out.println("La posición 7 corresponde a Agosto");
-		} else {
-			System.out.println("La posición 7 NO corresponde a Agosto");
-		}
+	void testTamano() {
+		assertEquals(12, meses.size());
+	}
+
+	// Comprobamos que el Array no es null
+	@Test
+	void testNul() {
+		assertTrue(meses != null);
+	}
+
+	// Comprobamos que en la posición 7 está agosto
+	@Test
+	void testPosicion() {
+		assertEquals("Agosto", meses.get(7));
 	}
 
 }
